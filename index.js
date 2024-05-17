6785c53e19a2edb31dc3bd66a2ddea0035295ba7
parthupman <parthupman@gmail.com>
2024-05-17 12:06:20 +0530

    setting up project

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 const { Telegraf } = require("telegraf");
+
+/*
+*  How to Obtain a Secret Token for Bot Creation
+
+* -1>On telegram Search for bot father type /start and press enter than press /newbot for creating new bot than give bot name that end with like computer_bot than it will ask for username ending with bot ex codeWithMe_Bot
+https://t.me/example_codeWithMe_Bot
+*
+
+
+
+
+
+*/
